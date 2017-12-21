d860deea647a23f5734b6bfb1dfeb502f988b0ca
Kamil Koziol <koziol1kamil@gmail.com>
2017-12-21 21:28:20 +0100

    BLOGAPP: * Added initial migrations * Added admin options
    OTHER:
    * Small update .gitignore
    * Small changes in PARENT_BASE_DIR and database settings

--- simpleBlog/simpleBlog/settings/base.py
+++ simpleBlog/simpleBlog/settings/base.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from pathlib import Path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PARENT_BASE_DIR = os.path.join("..", BASE_DIR)
+PARENT_BASE_DIR = str(Path(BASE_DIR).parent)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -80,7 +82,7 @@ WSGI_APPLICATION = 'simpleBlog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PARENT_BASE_DIR, 'simpleBlog_db.sqlite3'),
     }
 }
 
