8181296a5def8f2e810046490e7a5d410930aa2b
Kamil Kozioł <koziol1kamil@gmail.com>
2017-12-26 14:37:45 +0100

    BLOGAPP: * Added feed

--- simpleBlog/simpleBlog/blog/urls.py
+++ simpleBlog/simpleBlog/blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+
+from simpleBlog.blog.feeds import LatestPostsFeed
 from .views import PostListView, DetailPostView, SharePostView
 
 app_name = "blog"
@@ -11,4 +13,5 @@ urlpatterns = [
     path("<int:year>/<int:month>/<int:day>/<slug:slug>/",
          DetailPostView.as_view(), name="post_detail"),
     # path("tag/<slug:tag_slug>", PostListView.as_view(), name="post_list_by_tag")
+    path("feed/", LatestPostsFeed(), name="post_feed")
 ]
